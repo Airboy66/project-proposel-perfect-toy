253ae310461236ccc154087a1d6d9d0d317fd1ce
Aron Kohl <91220110+Airboy66@users.noreply.github.com>
2023-05-02 17:12:27 -0500

    FINAL

---
+++ Player.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Proposel___AronKohl
+{
+    public class Player
+    {
+        public int health { get; set; }
+        public int damage { get; set; }
+    }
+}
